9446f91822cf9512a8b772b51b908b7079e0465b
Shir Bar Lev <shirblc@gmail.com>
2022-04-28 10:29:24 +0300

    Added move functionality to RPS turn

--- rockPaperScissors/RPSTurn.h
+++ rockPaperScissors/RPSTurn.h
@@ -11,6 +11,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RPSTurn : NSObject
 
+typedef NS_ENUM(NSInteger, Move) {
+    Rock,
+    Paper,
+    Scissors
+};
+
+@property (nonatomic) Move move;
+
+-(instancetype)initWithMove: (Move)move;
+-(void)printMove;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- rockPaperScissors/RPSTurn.m
+++ rockPaperScissors/RPSTurn.m
@@ -9,4 +9,30 @@
 
 @implementation RPSTurn
 
+-(instancetype)initWithMove: (Move)move {
+    self = [super init];
+    
+    if(self) {
+        _move = move;
+    }
+    
+    return self;
+}
+
+-(void)printMove {
+    switch (_move) {
+        case Rock:
+            NSLog(@"You chose rock!");
+            break;
+            
+        case Paper:
+            NSLog(@"You chose paper!");
+            break;
+            
+        case Scissors:
+            NSLog(@"You chose scissors!");
+            break;
+    }
+}
+
 @end
