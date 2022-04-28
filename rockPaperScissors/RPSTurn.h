ce4437d6af707203cac83dc8fd47946d74abf941
Shir Bar Lev <shirblc@gmail.com>
2022-04-28 10:04:15 +0300

    Added class for RPS turns

---
+++ rockPaperScissors/RPSTurn.h
@@ -0,0 +1,16 @@
+//
+//  RPSTurn.h
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 28/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RPSTurn : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rockPaperScissors/RPSTurn.m
@@ -0,0 +1,12 @@
+//
+//  RPSTurn.m
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 28/04/2022.
+//
+
+#import "RPSTurn.h"
+
+@implementation RPSTurn
+
+@end
