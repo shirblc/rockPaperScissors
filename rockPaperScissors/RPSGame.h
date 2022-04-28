3561eed089e69edd7ae91a4d83bb008123512337
Shir Bar Lev <shirblc@gmail.com>
2022-04-28 10:34:22 +0300

    Added RPSGame class for handling game mechanism

---
+++ rockPaperScissors/RPSGame.h
@@ -0,0 +1,16 @@
+//
+//  RPSGame.h
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 28/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RPSGame : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rockPaperScissors/RPSGame.m
@@ -0,0 +1,12 @@
+//
+//  RPSGame.m
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 28/04/2022.
+//
+
+#import "RPSGame.h"
+
+@implementation RPSGame
+
+@end
