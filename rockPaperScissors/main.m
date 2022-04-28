3ddf6fffbc09ae8bcecc2501bfa11bfaf151273d
Shir Bar Lev <shirblc@gmail.com>
2022-04-28 09:59:37 +0300

    Initial Commit

---
+++ rockPaperScissors/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 28/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
