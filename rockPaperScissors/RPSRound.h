404867dbf7686ec52764f4b3289e5a12eef9072c
Shir Bar Lev <shirblc@gmail.com>
2022-04-29 11:53:55 +0300

    Added RPS Round class

---
+++ rockPaperScissors/RPSRound.h
@@ -0,0 +1,16 @@
+//
+//  RPSRound.h
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 29/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RPSRound : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ rockPaperScissors/RPSRound.m
@@ -0,0 +1,12 @@
+//
+//  RPSRound.m
+//  rockPaperScissors
+//
+//  Created by Shir Bar Lev on 29/04/2022.
+//
+
+#import "RPSRound.h"
+
+@implementation RPSRound
+
+@end
