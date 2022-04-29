c5b14a64d4e10ff1b5b66eaa5a02c04b253fb55b
Shir Bar Lev <shirblc@icloud.com>
2022-04-29 13:39:35 +0300

    Added game mechanism to main

--- rockPaperScissors/main.m
+++ rockPaperScissors/main.m
@@ -6,10 +6,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RPSGame.h"
+#import "RPSTurn.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        RPSGame *game = [[RPSGame alloc] init];
+        bool continueGame = true;
         
+        // While the user wants to continue, continue the game
+        while(continueGame) {
+            NSInteger userMove = [game getUserMove];
+            [game playRoundWithMove:userMove];
+            NSInteger shouldContinue = [game shouldContinueGame];
+            
+            if(shouldContinue == 0) {
+                continueGame = false;
+                NSLog(@"Ended game with score: User- %d, Computer- %d", game.userScore, game.computerScore);
+            }
+        }
     }
     return 0;
 }
