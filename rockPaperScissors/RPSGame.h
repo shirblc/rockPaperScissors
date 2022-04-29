8a7624053dd6b8d40aba80a4b4eea695e86e3cd6
Shir Bar Lev <shirblc@icloud.com>
2022-04-29 13:34:35 +0300

    Added helper methods for getting user input

--- rockPaperScissors/RPSGame.h
+++ rockPaperScissors/RPSGame.h
@@ -19,6 +19,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 -(instancetype)initWithMoveOne: (Move)moveOne;
 -(void)playRoundWithMove: (Move)move;
+-(NSInteger)getUserMove;
+-(NSInteger)shouldContinueGame;
 
 @end
 
--- rockPaperScissors/RPSGame.m
+++ rockPaperScissors/RPSGame.m
@@ -49,6 +49,33 @@
     }
 }
 
+-(NSInteger)getUserMove {
+    NSLog(@"RPS Round! Enter 0 (rock), 1 (paper) or 2 (scissors)");
+    sleep(5);
+    
+    NSInteger selection = [self getUserInput];
+    
+    return selection;
+}
+
+-(NSInteger)shouldContinueGame {
+    NSLog(@"Another round? Enter 0 for no, 1 for yes.");
+    sleep(5);
+    
+    NSInteger selection = [self getUserInput];
+    
+    return selection;
+}
+
+-(NSInteger)getUserInput {
+    NSFileHandle *handler = [NSFileHandle fileHandleWithStandardInput];
+    NSData *readData = handler.availableData;
+    NSString *userSelection = [[NSString alloc] initWithData:readData encoding:NSUTF8StringEncoding];
+    NSInteger selectionInt = [userSelection integerValue];
+    
+    return selectionInt;
+}
+
 /* Private Setters */
 -(void)setRoundNumber:(int)roundNumber {
     _roundNumber = roundNumber;
